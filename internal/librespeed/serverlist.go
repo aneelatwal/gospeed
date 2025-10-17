01b2001cfa40a0ab04aa4c410e7bf82f0c39ed15
aneelatwal <aneelatwal@gmail.com>
2025-10-17 10:32:16 -0700

    lists available servers

--- internal/librespeed/serverlist.go
+++ internal/librespeed/serverlist.go
@@ -1,1 +1,43 @@
 package librespeed
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type Server struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Sponsor  string  `json:"sponsor"`
+	URL      string  `json:"url"`
+	Lat      float64 `json:"lat"`
+	Lon      float64 `json:"lon"`
+	Distance float64 `json:"distance"`
+	Ping     float64 `json:"ping"`
+}
+
+func FetchServerList() ([]Server, error) {
+	resp, err := http.Get("https://librespeed.org/backend-servers/servers.php")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("failed to fetch server list: " + resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var servers []Server
+	if err := json.Unmarshal(body, &servers); err != nil {
+		return nil, err
+	}
+
+	return servers, nil
+}
