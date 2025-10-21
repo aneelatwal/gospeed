431f236f864248644e95501d9581dffabe4ae960
aneelatwal <aneelatwal@gmail.com>
2025-10-21 11:31:29 -0700

    refactoring

--- cmd/speedtest/main.go
+++ cmd/speedtest/main.go
@@ -7,19 +7,24 @@ import (
 	"github.com/aneelatwal/gospeed/internal/librespeed"
 )
 
+func PrintServerList(servers []librespeed.Server) {
+	for _, server := range servers {
+		fmt.Printf("Server ID: %d, Name: %s, URL: %s\n", server.ID, server.Name, server.ServerURL)
+	}
+}
+
 func main() {
+	// Get server list
 	servers, err := librespeed.FetchServerList()
 	if err != nil {
 		log.Fatalf("Error fetching server list: %v", err)
 	}
 
-	// for _, server := range servers {
-	// 	fmt.Printf("Server ID: %d, Name: %s, URL: %s\n", server.ID, server.Name, server.ServerURL)
-	// }
-
+	// Find fastest server
 	fastestServer := librespeed.PingServers(servers)
 	fmt.Printf("Fastest server is %s with average latency %v\n", fastestServer.Server.Name, fastestServer.Latency)
 
+	// Run download and upload tests
 	downloadSpeed, data, err := librespeed.RunDownloadTest(fastestServer)
 	if err != nil {
 		log.Fatalf("Error during download test: %v", err)
